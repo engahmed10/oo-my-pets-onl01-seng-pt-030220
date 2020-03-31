5547bd0af31dadc69f17ff8805ddb70913167bbf
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-31 01:41:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class Owner
+
+  attr_reader :name,:species
+
+ @@all =[]
+ @@count=0
+  def initialize(name)
+    @name=name
+    @species ="human"
+    @@all << self
+    @@count +=1
+  end
+
   # code goes here
+  def species
+     @species
+  end
+
+  def say_species
+      "I am a #{@species}."
+  end
+
+  def self.all
+     @@all
+  end
+
+  def self.count
+     @@count
+  end
+  def self.reset_all
+  #  @@all = 0
+    @@count =0
+  end
+
+  def cats
+     Cat.all.select do |cat|
+      cat.owner == self
+     end
+  end
+
+  def dogs
+    
+      Dog.all.select do |dog|
+       dog.owner == self
+      end
+  end
+
 end
